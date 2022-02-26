cde48081915297c0bbc8f2c1b9edb3c6b9f9dbf0
khushboobabil <khushboobabil12@gmail.com>
2022-02-26 19:27:53 +0530

    mongoose assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+const userController= require("../controllers/userController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
-router.post("/createUser", UserController.createUser  )
+router.post("/newBook", userController.newBook  )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getAllBooksList", userController.getAllBooks)
 
 module.exports = router;
